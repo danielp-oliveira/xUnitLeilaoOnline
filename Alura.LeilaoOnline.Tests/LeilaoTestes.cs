ee93580452006544e702c7523d6313b5bcb05519
Daniel Oliveira <danielp.oliveira@outlook.com>
2020-07-12 22:10:55 -0300

    Testando Leilao com varios lances/unico lance

---
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,45 @@
+using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests {
+    public class LeilaoTestes {
+        [Fact]
+        public void LeilaoComVariosLances () {
+            //Given
+            var leilao = new Leilao ("Van Gogh");
+            var fulano = new Interessada ("Fulano", leilao);
+            var maria = new Interessada ("Maria", leilao);
+
+            leilao.RecebeLance (fulano, 800);
+            leilao.RecebeLance (maria, 900);
+            leilao.RecebeLance (fulano, 1000);
+            leilao.RecebeLance (maria, 990);
+
+            //When
+            leilao.TerminaPregao ();
+
+            //Then
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal (valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance () {
+            //Given
+            var leilao = new Leilao ("Van Gogh");
+            var fulano = new Interessada ("Fulano", leilao);
+
+            leilao.RecebeLance (fulano, 800);
+
+            //When
+            leilao.TerminaPregao ();
+
+            //Then
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal (valorEsperado, valorObtido);
+        }
+    }
+}
